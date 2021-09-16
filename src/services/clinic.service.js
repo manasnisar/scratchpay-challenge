e925c65d0b185ad3f7032b0bcf6d8446a8d383a5
manasnisar <anasnisar980@gmail.com>
2021-09-16 15:31:44 +0500

    Documenation added inline documentation for clinic service

--- src/services/clinic.service.js
+++ src/services/clinic.service.js
@@ -3,27 +3,33 @@ const axios = require('axios');
 const ApiError = require('../utils/ApiError');
 const { doesUserWant } = require('../utils/clinic-utils');
 
-// /**
-//  * Create a user
-//  * @param {Object} userBody
-//  * @returns {Promise<Clinic>}
-//  */
+/**
+ * Get clinics
+ * @param {Object} Query
+ * @returns {Promise<Object>}
+ */
 
 const getClinics = async (query) => {
+  // prepare get request to fetch clinics from both apis
   const dentalClinicsRequest = axios.get('https://storage.googleapis.com/scratchpay-code-challenge/dental-clinics.json');
   const vetClinicsRequest = axios.get('https://storage.googleapis.com/scratchpay-code-challenge/vet-clinics.json');
   let clinics;
   try {
+    // Execute the requests and merge responses
     clinics = await axios.all([dentalClinicsRequest, vetClinicsRequest]);
     clinics = [...clinics[0].data, ...clinics[1].data];
   } catch (error) {
+    // In case the resource couldn't be fetched
     throw new ApiError(httpStatus.NO_CONTENT, 'Clinics not found!');
   }
+  // Get the query fields before hand to avoid getting them repeadetly
   const queryFields = Object.keys(query);
 
+  // filter the results based on query
   clinics = clinics.filter((clinic) => {
     return doesUserWant(query, queryFields, clinic);
   });
+  // return the queried resources
   return clinics;
 };
 
