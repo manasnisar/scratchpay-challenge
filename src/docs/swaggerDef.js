6e08cebeb1210c9926e3c9c508b601291a2a4330
Anas Nisar <55254721+manasnisar@users.noreply.github.com>
2021-09-16 19:23:51 +0500

    Update swaggerDef.js

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -9,7 +9,7 @@ const swaggerDef = {
   },
   servers: [
     {
-      url: `http://localhost:${config.port}/v1`,
+      url: `${config.env === "production" ? "https://scratchpay-challenge.herokuapp.com:" : "http://localhost:" }${config.port}/v1`,
     },
   ],
 };
